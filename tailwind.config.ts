1a8e93d6bd0ea704df87e39f5b0cd63f733208b9
Gabriel Siqueira <gabrielss2406@gmail.com>
2025-03-14 20:32:47 -0300

    fix: empty file

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        './src/**/*.{html,js,jsx,tsx}',  // Adapte conforme a estrutura do seu projeto
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#ff5733',  // Exemplo: você pode adicionar cores personalizadas aqui
+            },
+        },
+    },
+    plugins: [],
+}
